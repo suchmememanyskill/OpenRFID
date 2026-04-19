64daf37cc3ab36f8a0bb3f23cb27770e0ef0f183
yanshay <yanshay>
2026-04-19 20:25:26 +0300

    Remove log + move relevant code to general ndef processing

--- src/tag/ndef_tag_processor.py
+++ src/tag/ndef_tag_processor.py
@@ -14,6 +14,45 @@ NDEF_ERR = -1
 NDEF_PARAMETER_ERR = -2
 NDEF_NOT_FOUND_ERR = -3
 
+NDEF_URI_PREFIX_MAP = {
+    0x00: "",
+    0x01: "http://www.",
+    0x02: "https://www.",
+    0x03: "http://",
+    0x04: "https://",
+    0x05: "tel:",
+    0x06: "mailto:",
+    0x07: "ftp://anonymous:anonymous@",
+    0x08: "ftp://ftp.",
+    0x09: "ftps://",
+    0x0A: "sftp://",
+    0x0B: "smb://",
+    0x0C: "nfs://",
+    0x0D: "ftp://",
+    0x0E: "dav://",
+    0x0F: "news:",
+    0x10: "telnet://",
+    0x11: "imap:",
+    0x12: "rtsp://",
+    0x13: "urn:",
+    0x14: "pop:",
+    0x15: "sip:",
+    0x16: "sips:",
+    0x17: "tftp:",
+    0x18: "btspp://",
+    0x19: "btl2cap://",
+    0x1A: "btgoep://",
+    0x1B: "tcpobex://",
+    0x1C: "irdaobex://",
+    0x1D: "file://",
+    0x1E: "urn:epc:id:",
+    0x1F: "urn:epc:tag:",
+    0x20: "urn:epc:pat:",
+    0x21: "urn:epc:raw:",
+    0x22: "urn:epc:",
+    0x23: "urn:nfc:",
+}
+
 class NdefRecord:
     def __init__(self, payload: bytes, tnf: int, record_type: str = "", mime_type: str = ""):
         self.tnf = tnf
--- src/tag/spoolease/processor.py
+++ src/tag/spoolease/processor.py
@@ -1,49 +1,9 @@
 from filament import GenericFilament
 from reader.scan_result import ScanResult
-from tag.ndef_tag_processor import NdefRecord, NdefTagProcessor
+from tag.ndef_tag_processor import NDEF_URI_PREFIX_MAP, NdefRecord, NdefTagProcessor
 import urllib.parse
 
 from . import constants as Constants
-
-
-NDEF_URI_PREFIX_MAP = {
-    0x00: "",
-    0x01: "http://www.",
-    0x02: "https://www.",
-    0x03: "http://",
-    0x04: "https://",
-    0x05: "tel:",
-    0x06: "mailto:",
-    0x07: "ftp://anonymous:anonymous@",
-    0x08: "ftp://ftp.",
-    0x09: "ftps://",
-    0x0A: "sftp://",
-    0x0B: "smb://",
-    0x0C: "nfs://",
-    0x0D: "ftp://",
-    0x0E: "dav://",
-    0x0F: "news:",
-    0x10: "telnet://",
-    0x11: "imap:",
-    0x12: "rtsp://",
-    0x13: "urn:",
-    0x14: "pop:",
-    0x15: "sip:",
-    0x16: "sips:",
-    0x17: "tftp:",
-    0x18: "btspp://",
-    0x19: "btl2cap://",
-    0x1A: "btgoep://",
-    0x1B: "tcpobex://",
-    0x1C: "irdaobex://",
-    0x1D: "file://",
-    0x1E: "urn:epc:id:",
-    0x1F: "urn:epc:tag:",
-    0x20: "urn:epc:pat:",
-    0x21: "urn:epc:raw:",
-    0x22: "urn:epc:",
-    0x23: "urn:nfc:",
-}
 
 
 class SpooleaseTagProcessor(NdefTagProcessor):
@@ -57,7 +17,6 @@ class SpooleaseTagProcessor(NdefTagProcessor):
                 if filament is not None:
                     return filament
 
-        self.logger.error("SpoolEase processing failed: No valid SpoolEase NDEF URI record found")
         return None
 
     def __parse_spoolease_payload(self, payload: bytes) -> GenericFilament | None:
